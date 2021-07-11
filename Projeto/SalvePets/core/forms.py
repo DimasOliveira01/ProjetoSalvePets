c5ac4534c173360dd34d201f02a4493d94818518
Matheus Costa <matheus.m.costa@bradesco.com.br>
2021-07-11 12:40:12 -0300

    Update no arquivo forms.py

--- Projeto/SalvePets/core/forms.py
+++ Projeto/SalvePets/core/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Pet
 
+# Arquivo para criação dos nossos FORMS caso prefiram.
+
 class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = '__all__'
+
+
+class MeuCadastroForm(forms.Form):
+	first_name = forms.CharField(max_length = 50)
+	last_name = forms.CharField(max_length = 50)
+	email_address = forms.EmailField(max_length = 150)
+	#message = forms.CharField(widget = forms.Textarea, max_length = 2000)
