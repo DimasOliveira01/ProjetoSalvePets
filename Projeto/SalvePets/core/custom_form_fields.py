0adc378feda426d5353c99df80821ffea79ecd2b
Matheus Costa <matheus.m.costa@bradesco.com.br>
2021-08-01 14:12:58 -0300

    Ajustes finais para CPF e Telefone
    Mais importante: campos de CPF e Telefone do Cadastro do usuário estão OK.
    URL "completar-cadastro" ainda não funcional.
    Comentei o ajuste fake no index e outras alterações ainda inúteis.

---
+++ Projeto/SalvePets/core/custom_form_fields.py
@@ -0,0 +1,106 @@
+from django import forms
+from django.core import validators
+from django.forms import ValidationError
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.utils.translation import ugettext_lazy as _
+
+
+# Campo de Form criado especificamente como validador do CPF - terá de ser alterado futuramente para validar CNPJ
+
+class CpfCnpjField(forms.CharField):
+    def to_python(self, value):
+        # Limpa a máscara puxando só valores numéricos
+        if not value:
+            return []
+        else:
+            value = value.replace('.','')
+            value = value.replace('-','')
+        return value    
+
+    def validate(self, value):
+        # Validador do CPF em si
+        # Ele só funciona porque o Python automaticamente roda o método acima antes, que faz a "limpa"
+        super().validate(value)
+        tamCPF = 11
+        i=0
+        j=10
+        digito1 = 0
+        digito2 = 0
+        tam = len(value)
+        if tam!=tamCPF:
+            if tam<tamCPF:
+                raise ValidationError(
+                    _('Número de dígitos (%d) inferor ao esperado: %d' % (tam, tamCPF)),
+                    code='TamDivergente',
+                    params={'value': '11'},
+                )
+            else:
+                raise ValidationError(
+                    _('Número de dígitos (%d) superior ao esperado: %d' % (tam, tamCPF)),
+                    code='TamDivergente',
+                    params={'value': '11'},
+                )
+        else:
+            if (value=='00000000000') or (value=='11111111111') or (value=='22222222222') or (value=='33333333333') or (value=='44444444444') or (value=='55555555555'):
+                raise ValidationError(
+                    _('CPF inválido. Números identicos.'),
+                    code='NrosIguais',
+                    params={'value': '11'},
+                )
+            arr = list(value)
+            #Validação do primeiro dígito
+            for i in range(0,len(arr)-2):
+                digito1 = digito1 + int(arr[i])*(j-i)
+            digito1 %= 11
+            if(digito1 < 2):
+                digito1 = 0
+            else:
+                digito1 = 11 - digito1
+            #Validação do segundo dígito
+            i=0
+            j=11
+            for i in range(0,len(arr)-1):
+                digito2 = digito2 + int(arr[i])*(j-i)
+            digito2 %= 11
+            if(digito2 < 2):
+                digito2 = 0
+            else:
+                digito2 = 11 - digito2
+            if not(arr[9]==str(digito1) and arr[10]==str(digito2)):
+                raise ValidationError(
+                    _('CPF inválido.'),
+                    code='Invalido',
+                    params={'value': '01'},
+                )
+
+class TelefoneField(forms.Field):
+    def to_python(self, value):
+        # Limpa a máscara puxando só valores numéricos
+        if not value:
+            return []
+        else:
+            value = value.replace('(','')
+            value = value.replace(')','')
+            value = value.replace('-','')
+            value = value.replace(' ','')
+        return value
+
+    def validate(self, value):
+        # Validador do Telefone (apenas número de Dígitos)
+        super().validate(value)
+        tamTelefone = 11
+        tam = len(value)
+        if tam!=tamTelefone:
+            if tam<tamTelefone:
+                raise ValidationError(
+                    _('Número de dígitos (%d) inferor ao esperado: %d' % (tam, tamTelefone)),
+                    code='TamDivergente',
+                    params={'value': '11'},
+                )
+            else:
+                    raise ValidationError(
+                    _('Número de dígitos (%d) superior ao esperado: %d' % (tam, tamTelefone)),
+                    code='TamDivergente',
+                    params={'value': '11'},
+                )
--- Projeto/SalvePets/core/forms.py
+++ Projeto/SalvePets/core/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Pet, USUARIO, User
+from .custom_form_fields import CpfCnpjField, TelefoneField
 from django.utils.translation import ugettext_lazy as _
+
+
+# Criar Array de Ano de Nascimento - não sei se faz sentido mover pra arquivo separado..
 
 BIRTH_YEAR_CHOICES  = []
 for i in range(1900,2021):
     BIRTH_YEAR_CHOICES.append(str(i))
+
+# Forms - pra mim isso aqui é o "Controller" do projeto. Masterizar isso é garantia de sucesso.
 
 class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label=_('Nome:'))
@@ -26,13 +32,13 @@ class UserForm(forms.ModelForm):
         fields = ('email', 'first_name', 'last_name')
 
 class UsuarioForm(forms.ModelForm):
-    #cpfCnpj = forms.CharField(max_length=14, label='CPF')
+    cpfCnpj = CpfCnpjField(label='CPF')
     dataNascimento = forms.DateField(required=True, widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES),input_formats=['%d/%m/%Y'], label='Data de Nascimento')
-    #telefone = forms.CharField(required=False, max_length=16, label='Nro de Telefone')
+    telefone = TelefoneField(required=False, label='Nro de Telefone')
     #receberNotificacoes = forms.BooleanField(required=False, label = 'Desejo receber notificações', help_text='(Marque este campo caso deseje ser notificado sobre pets perdidos ou encontrados.)')
     class Meta:
         model = USUARIO
-        fields = ('cpfCnpj', 'dataNascimento', 'telefone', 'site', 'receberNotificacoes') #Retirei tipoUsuairo e site
+        fields = ('cpfCnpj', 'dataNascimento', 'telefone', 'site', 'receberNotificacoes') #Retirei tipoUsuairo
 
 
 #Classe que seria usada como "Cadastro Único" - li que é possível fazer funcionar, só preciso entender melhor como.
