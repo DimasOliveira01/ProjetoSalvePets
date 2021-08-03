63bea456a55adbccd8772c7af525013650a8e86f
root <root@ip-172-31-9-188.ec2.internal>
2021-08-03 13:39:45 +0000

    ultima versao deploy mvp 03-08-2021

--- Projeto/SalvePets/SalvePets/settings.py
+++ Projeto/SalvePets/SalvePets/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-99x#o@c^zab4yjx^t96nq!zniee1+r7fr*@7duka^3-vm2+iln
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.salvepets.online','salvepets.online','3.239.208.137','ec2-3-239-208-137.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['www.salvepets.online','salvepets.online','18.233.200.207','ec2-18-233-200-207.compute-1.amazonaws.com']
 
 #Caminho das imagens(foto)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
