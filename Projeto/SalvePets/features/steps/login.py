adb80d6077675c79ab00b08f3dfa40eba694fd06
Matheus Dias <m.augusto.nd@gmail.com>
2021-08-10 09:18:45 -0300

    Correções

--- Projeto/SalvePets/features/steps/login.py
+++ Projeto/SalvePets/features/steps/login.py
@@ -2,7 +2,7 @@ from behave import *
 
 @given('que o usuario insira e-mail e senha corretos')
 def step(context):
-    context.browser.get('http://127.0.0.1:8000/pt-br/accounts/login/')
+    context.browser.get('https://salvepets.online/pt-br/accounts/login/')
     #context.browser.find_element_by_id('id_login').send_keys('salvepets_superuser')
     #context.browser.find_element_by_id('id_password').send_keys('salve123456')
     context.browser.find_element_by_id('id_login').send_keys('123')
